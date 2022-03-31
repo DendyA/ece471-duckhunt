2ee3e26773d37943c3f3903a57450902d83b17a2
JonEB <87618690+Jon-EB@users.noreply.github.com>
2022-03-31 11:40:26 -0700

    Add files via upload

--- solution.py
+++ solution.py
@@ -13,6 +13,9 @@ Manual mode where you can use your mouse as also been added for testing purposes
 def GetLocation(move_type, env, current_frame):
     # time.sleep(1) #artificial one second processing time
     visionTypeToUse = comp_vis_type[0]
+
+    # keep previous frame - JE
+    global prev_frame
     
     #Use relative coordinates to the current position of the "gun", defined as an integer below
     if move_type == "relative":
@@ -36,13 +39,25 @@ def GetLocation(move_type, env, current_frame):
         Bottom right = (W, H) 
         """
         if visionTypeToUse == comp_vis_type[0]:
-            birdEye = cv2.imread("imgs/template_eye.png", cv2.IMREAD_GRAYSCALE)
+            greyScaleFrame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2GRAY)
+            
+            birdEye = cv2.imread("template_eye.png", cv2.IMREAD_GRAYSCALE)
 
             # Should be in WxHxD tuple form. (Where W is width, h is height, and d is depth (num of channels)).
             birdEyeShape = birdEye.shape
             currentFrameShape = current_frame.shape
 
-            greyScaleFrame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2GRAY)
+            # find and remove background - JE
+            if 'prev_frame' not in globals():
+                print("Set Prev frame")
+                prev_frame = greyScaleFrame
+            diff_frame = np.where(greyScaleFrame == prev_frame,0,greyScaleFrame)
+            total = sum(sum(diff_frame))
+            if (total > 50000): # needs fix? triggered if non static background like rain - JE
+                print("Set Prev frame, total: ",total)
+                prev_frame = greyScaleFrame
+            prev_frame = greyScaleFrame
+            greyScaleFrame = diff_frame 
 
             isBird = cv2.matchTemplate(image=greyScaleFrame, templ=birdEye, method=cv2.TM_CCOEFF)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(isBird)
@@ -55,4 +70,3 @@ def GetLocation(move_type, env, current_frame):
             coordinate = env.action_space_abs.sample()
 
     return [{'coordinate' : coordinate, 'move_type' : move_type}]
-
