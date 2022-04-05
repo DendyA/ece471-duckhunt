70c289481d3c30725fbca0a9586172b64b474f37
Mark Kaiser <markksr5@gmail.com>
2022-04-05 11:31:45 -0700

    adding models to repo

--- setup_yolo.sh
+++ setup_yolo.sh
@@ -2,8 +2,12 @@ git clone https://github.com/ultralytics/yolov5.git
 
 cd yolov5
 
+mkdir runs
+
 pip3 install -r requirements.txt
 
 cd ..
 
 cp detect_local.py ./yolov5/detect_local.py
+
+cp -r train ./yolov5/runs/
